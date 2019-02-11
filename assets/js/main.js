001121d1544b67ffdf94cdd55007973c6ccb23f9
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-02-11 09:59:06 +0100

    wrote it into the DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,4 @@ function sumup(array) {
   return array.reduce((accumulator, value) => accumulator + value);
 }
 let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(sumup(myArray));
+document.write(sumup(myArray));
